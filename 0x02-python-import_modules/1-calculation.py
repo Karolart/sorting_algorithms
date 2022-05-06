20411fd5f05d6550ec19f76a211429418b9f786b
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-05-05 23:32:03 -0500

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print("{ } + { } = { }".format(a, b, add(a, b))\n
-           { } - { } = { }".format(a, b, sub(a, b))\n 
-           { } * { } = { }".format(a, b, mul(a, b))\n
-           { } / { } = { }".format(a, b, div(a, b))")
+          "{ } - { } = { }".format(a, b, sub(a, b))\n 
+          "{ } * { } = { }".format(a, b, mul(a, b))\n
+          "{ } / { } = { }".format(a, b, div(a, b)))
