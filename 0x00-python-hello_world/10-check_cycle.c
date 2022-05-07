c99d86d1e30fafa7ab258779740662ec99f00c5d
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-05-07 01:14:48 -0500

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,22 +4,21 @@
  *:list: pointer will be check
  *return: 1 if is a cycle or 0 if it is not
 */
- int check_cycle(listint_t *list)
- {
-      listint_t *skilpad;
-      listint_t *haas;
-      
-       if (list == NULL || list->next == NULL)
-               return (0);
-             
-       skilpad = haas = list;
-        
-       while (skilpad && haas && haas->next)
-         {
-             skilpad = skilpad->next;
-             hass = hass->next->;
-             if (skilpad == hass)
-                    return(1);
-        }
-        return (0);
- }
+int check_cycle(listint_t *list)
+{
+	listint_t *head, *current;
+
+	head = list;
+	current = list;
+
+	while (head && current && current->next)
+	{
+		current = current->next->next;
+		if (head == current)
+			return (1);
+
+		head = head->next;		
+	}
+
+	return (0);
+}
