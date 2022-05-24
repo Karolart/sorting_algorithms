250117a8ee79d402c2951b58e077e5beb50a285c
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-05-23 22:52:25 -0500

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
 
     c = 0
@@ -8,11 +7,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             if c < x:
-            print ('{}'.format(my_list[element]) , end= '')
+            print ('{}'.format(my_list[c]) , end= '')
             c += 1
 
         print()
     except TypeError:
         pass
     finally:
-        return element
+        return c
