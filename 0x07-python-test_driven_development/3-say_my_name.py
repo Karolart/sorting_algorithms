c21afc8aeac7aeb172b295948737a999162a8ded
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-05-31 23:24:13 -0500

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 """
-This module prints the name of a anything
-Output: 'My name is <first name> <last name>'
-    * The first name and last name must be a strings
+This is the "3-say_my-name" module.
+The 3-say_my_name  module supplies one function, say_my_name.
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints the names
-    Args:
-        first_name (str): The first name of anything.
-        last_name (str): The last name of anything.
-    Raises:
-        TypeError: If `first_name` and `last_name` aren't strings.
-    """
-
+    """Prints "My name is" followed by the first name and optional last name"""
     if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
-
+        raise TypeError("first_name must be a string")
     if type(last_name) is not str:
-        raise TypeError('last_name must be a string')
-
-    print('My name is', first_name, last_name)
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
