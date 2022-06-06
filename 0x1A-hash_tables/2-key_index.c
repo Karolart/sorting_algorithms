bec19073eb58ef8b72ff974843d1a7cd96e0c40d
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-06-05 23:34:05 -0500

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,7 +3,7 @@
 * key_index - hash table set
 * @key: the key
 * @size: size value
-* Return: the place the value is store in the array 
+* Return: the place the value is store in the array
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
