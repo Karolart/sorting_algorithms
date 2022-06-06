ea1e47d7c1962a9cd6c2ad1001cd893dc5c60e8f
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-06-05 23:58:34 -0500

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,8 +1,10 @@
-#include <stdlib.h>
 #include "hash_tables.h"
+
 /**
- * hash_table_delete - delete yo hashes
- * @ht: hash table for deletion
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ *
+ * Return: void
  */
 void hash_table_delete(hash_table_t *ht)
 {
