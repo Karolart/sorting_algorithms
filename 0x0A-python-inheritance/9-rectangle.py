654c724b181b4f761a8d39a9f549d304b4b7f929
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-06-08 23:18:34 -0500

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-# 9-rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
+    """BaseGeometry using"""
 
     def __init__(self, width, height):
         """Intialize a new Rectangle.
         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
+            width (int): new Rectangle width.
+            height (int): new Rectangle height.
         """
         super().integer_validator("width", width)
         self.__width = width
