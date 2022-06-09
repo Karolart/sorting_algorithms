d1b87746c9bf4e7eb497a0ee1243a22c8f78c307
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-06-08 19:19:06 -0500

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,9 +2,9 @@
 """
 contains a def is_kind_of_class function
 """
+
+
 def is_kind_of_class(obj, a_class):
-
-
     """
     check if the object is an instance or if the object is an instance
     """
