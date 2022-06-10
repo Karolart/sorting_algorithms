ac4f765b6ab8e7fc2fb862a941cad8b19c7bf87c
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-06-09 22:06:06 -0500

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-"""
-Contains the "read_lines" function
-"""
-
-
-def read_lines(filename="", nb_lines=0):
-    """reads the lines of file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding='utf-8') as f:
-        if nb_lines <= 0:
-            print(f.read(), end='')
-            return
-        i = 0
-        for i, line in enumerate(f):
-            if i == nb_lines:
-                break
-            print(line, end='')
+def append_write(filename="", text=""):
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
